a7481379a275d885efbb7cefa71a5670c1cf2195
mattjared <mattjared9@gmail.com>
2015-07-02 21:32:50 -0500

    adding today's canvas fun

---
+++ _harp/014/script.js
@@ -0,0 +1,16 @@
+var canvas = document.querySelector("#c1");
+var context = canvas.getContext('2d');
+context.fillStyle = 'blue';
+context.fillRect(10, 10, 100, 100);
+
+var cx = document.querySelector('#c2').getContext('2d');
+cx.strokeStyle = "blue";
+cx.strokeRect(5, 5, 50, 50);
+cx.lineWidth = 15;
+cx.strokeRect(135, 5, 150, 150);
+
+var cx = document.querySelector('#c2').getContext('2d');
+cx.beginPath();
+for (var y = 10; y  ) {
+
+}
