e2b08119b1bf9d20267e97a1a7d4736842759fc2
mattjared <mattjared9@gmail.com>
2015-07-07 23:41:03 -0500

    adding

--- 019/script.js
+++ 019/script.js
@@ -9,3 +9,17 @@ for (var n in myHashTable) {
     document.writeln("<p>" + n + ": " +  myHashTable[n] + "</p>")
   }
 }
+
+var myObj = {
+  name: "Sneaky W. Roberts",
+  'goto': "jail",
+  grade: 'A',
+  level: 3
+};
+
+var matt = new Object();
+
+matt.living = true;
+matt.age = 27;
+matt.gender = 'male';
+document.writeln(matt.living);
