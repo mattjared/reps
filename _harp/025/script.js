76262619f88e1ab7bea0e16f3fe298a758d97491
mattjared <mattjared9@gmail.com>
2015-07-13 20:22:13 -0500

    adding

---
+++ _harp/025/script.js
@@ -0,0 +1,17 @@
+document.querySelector('#test1').blur();
+console.log(document.activeElement.tagName);
+
+function clicker() {
+    console.log("hello");
+}
+
+var form = document.querySelector('form');
+console.log(form.elements[2].type);
+console.log(form.elements.password.type);
+console.log(form.elements.name.type == form);
+
+var form1 = document.querySelector('#form1');
+form1.addEventListener('submit', function(event){
+  console.log("Saving value". form1.elements.value.value);
+  e.preventDefautlt();
+});
