d6cf8a6e11ed3a01540b2c7e31ff033a981aefb4
mattjared <mattjared9@gmail.com>
2015-07-17 17:25:32 -0500

    adding

--- 026/script.js
+++ 026/script.js
@@ -5,13 +5,21 @@ function clicker() {
     console.log("hello");
 }
 
-// var form = document.querySelector('form');
-// console.log(form.elements[2].type);
-// console.log(form.elements.password.type);
-// console.log(form.elements.name.type == form);
-
 var form = document.querySelector('form');
-form.addEventListener('submit', function(event){
-  console.log("Saving value", form.elements.value.value);
+console.log(form.elements[2].type);
+console.log(form.elements.password.type);
+console.log(form.elements.name.type == form);
+
+var form1 = document.querySelector('#form1');
+form1.addEventListener('submit', function(event){
+  console.log("Saving value", form1.elements.value.value);
   event.preventDefault();
+});
+
+var textarea = document.querySelector('textarea');
+textarea.addEventListener('keydown', function(event){
+  if (event.keyCode == 113) {
+    replaceSelection(textarea, "Khasekhemwy");
+    event.preventDefault();
+  }
 });
