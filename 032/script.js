ee739839f24a7615f069345938739b5bf848b790
mattjared <mattjared9@gmail.com>
2015-07-22 20:39:45 -0500

    adding

--- 032/script.js
+++ 032/script.js
@@ -1,8 +1,10 @@
+document.querySelector('#first').focus();
+
 function doMath(){
   var first = parseInt(document.querySelector('#first').value);
   var second = parseInt(document.querySelector('#second').value);
   var third = first + second;
-  document.write(third);
+  document.querySelector('#answer').innerHTML = third;
 }
 
 
