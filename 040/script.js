5a503124f5762360aed1a3fec792981979a4aea9
mattjared <mattjared9@gmail.com>
2015-11-09 09:55:38 -0600

    Get your reps in

--- 040/script.js
+++ 040/script.js
@@ -1,5 +1,12 @@
 $('button').on('click', function() {
     var x = $('.findClass').val();
     var y = $('.placeClass').val();
-    $('.findme').addClass(y);
+    $('.' + x).addClass(y);
 });
+
+// var b = document.getElementById('button')
+
+// document.getElementById('button').onclick = function () {
+//     console.log(b);
+// }
+
